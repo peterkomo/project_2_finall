4a5c0a78044f9e4a683c754b7cad5742d1a44733
peterkomo <mwangipeterkomo@gmail.com>
2023-09-10 23:57:54 +0300

    importing nessesry lbrarys

---
+++ main.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+#importng nessesary libraries
