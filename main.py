c7ca34f9195934c5b4e2f227f26016ac6c60dda7
peterkomo <mwangipeterkomo@gmail.com>
2023-09-10 23:59:25 +0300

    declarative base

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 #importng nessesary libraries
+
+Base = declarative_base()
+#declarative base for sql alchemy
